c7c2261cc31bb449ca3cb4b48ea438f88787b25e
Yi Zhong <maryer1994@gmail.com>
2018-03-09 01:13:19 +0800

    前端加密传输

--- src/main/resources/js/websocket.js
+++ src/main/resources/js/websocket.js
@@ -40,6 +40,7 @@ function login() {
     user = {};
     user.username = document.getElementById("username").value;
     user.password = document.getElementById("password").value;
+    user.password = encrypt(user.password);
     message = LOGIN + SPLIT + SPLIT + JSON.stringify(user);
     console.log(message);
     send(message);
@@ -57,4 +58,17 @@ function checkLoginUsers() {
     message = LOGIN_USERS + SPLIT + SPLIT;
     console.log(message);
     send(message);
+}
+
+function encrypt(msg) {
+    let publicKeyString = '-----BEGIN PUBLIC KEY-----\n' +
+        'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDneEL13J6R31bjtdKpn6NiwyVC\n' +
+        '4Tr0cKxQibt3mcZpRP88U3ronjuYgIcEVFISqBdL2k+8BboZX0O0a81/nF3e4dDN\n' +
+        'wpZ39JLDgHlwYlJNF7NORrxrSnVDByVIE5bN6vzBR/hi/W1/kR+aOJBEDQjL2X3n\n' +
+        '73hwKxvn6rTs0OR4ewIDAQAB\n' +
+        '-----END PUBLIC KEY-----';
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKeyString);
+    var encrypted = encrypt.encrypt(msg);
+    return encrypted;
 }
